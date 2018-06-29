b516ebee9c93a2b00ba02df9ea966a5e313ca07e
antalmega <antalmega@gmail.com>
2018-06-29 16:35:43 +0200

    added Message Form, create and delete messages

--- warbler/warbler-client/src/components/MessageItem.js
+++ warbler/warbler-client/src/components/MessageItem.js
@@ -3,7 +3,14 @@ import Moment from "react-moment";
 import { Link } from "react-router-dom";
 import DefaultProfileImg from "../images/default-profile-image.jpg";
 
-const MessageItem = ({ date, text, username, profileImageUrl }) => (
+const MessageItem = ({
+  date,
+  text,
+  username,
+  profileImageUrl,
+  removeMessage,
+  isCorrectUser
+}) => (
   <div>
     <li className="list-group-item">
       <img
@@ -21,6 +28,11 @@ const MessageItem = ({ date, text, username, profileImageUrl }) => (
           </Moment>
         </span>
         <p>{text}</p>
+        {isCorrectUser && (
+          <a className="btn btn-danger" onClick={removeMessage}>
+            Delete
+          </a>
+        )}
       </div>
     </li>
   </div>
